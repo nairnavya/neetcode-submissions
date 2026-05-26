418a9946749e12d7516aec6ff5d705f79b6ff25c
Navya Nair <nairnavya@gmail.com>
2026-05-26 08:58:52 -0500

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.py
@@ -0,0 +1,25 @@
+class TimeMap:
+
+    def __init__(self):
+        self.keyStore = {}
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.keyStore:
+            self.keyStore[key] = {}
+        if timestamp not in self.keyStore[key]:
+            self.keyStore[key][timestamp] = value
+        self.keyStore[key][timestamp] = value
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.keyStore:
+            return ""
+        seen = 0
+
+        for time in self.keyStore[key]:
+            if time <= timestamp:
+                if seen > time:
+                    seen = seen
+                else:
+                    seen = time
+        return "" if seen == 0 else self.keyStore[key][seen]
+
